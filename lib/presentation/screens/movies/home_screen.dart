7a034ecdcfc80b18733cac90cdc4cc8e94948364
Alejandro Loayza <alexloayza13@gmail.com>
2023-07-12 21:18:02 +0200

    swiper implementado

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -34,18 +35,31 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   @override
   Widget build(BuildContext context) {
 
-    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    // final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    final slideShowMovies = ref.watch(moviesSlideShowProvider);
 
-    if(nowPlayingMovies.isEmpty) return const Center(child: CircularProgressIndicator());
+    if(slideShowMovies.isEmpty) return const Center(child: CircularProgressIndicator());
 
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index) {
-        final movie = nowPlayingMovies[index];
-        return ListTile(
-          title: Text(movie.title),
-        );
-      },
+    return Column(
+      children: [
+
+        const CustomAppBar(),
+
+        MoviesSlideShow(movies: slideShowMovies),
+
+        // Expanded( // uso expanded para no usar el sizebox o container (otras opciones: sizebox.expande, sizebox.shrink)
+        //   child: ListView.builder(
+        //     itemCount: nowPlayingMovies.length,
+        //     itemBuilder: (context, index) {
+        //       final movie = nowPlayingMovies[index];
+        //       return ListTile(
+        //         title: Text(movie.title),
+        //       );
+        //     },
+        //   ),
+        // ),
+
+      ],
     );
   }
 }
